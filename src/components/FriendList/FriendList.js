5c6c738e07f25ab7a84dffe1930cfb9befed18cc
Pavel Mogilevskyi <pavel13595@gmail.com>
2022-11-28 17:20:04 +0200

    code

---
+++ src/components/FriendList/FriendList.js
@@ -0,0 +1,25 @@
+import { FriendListItem } from 'components/FriendListItem/FriendListItem';
+import PropTypes from 'prop-types';
+
+export const FriendList = ({ friends }) => {
+  return (
+    <ul className="friend-list">
+      {friends.map(({ avatar, name, isOnline, id }) => (
+        <FriendListItem
+          key={id}
+          avatar={avatar}
+          name={name}
+          isOnline={isOnline}
+        />
+      ))}
+    </ul>
+  );
+};
+
+FriendList.propTypes = {
+  friends: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.number.isRequired,
+    })
+  ),
+};
